a0ad7dd03d2e59b4a2c237b2b875e5208112fe2a
Shu12388y <spl.sp999@gmail.com>
2024-07-24 20:44:24 +0530

    file added

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -19,7 +19,7 @@ function Page() {
                 <div className='relative w-full h-[100vh] flex flex-col items-center justify-center overflow-x-hidden'>
                     <div>
                         <h1 className='text-white font-bold text-5xl text-center animate__animated animate__backInLeft'>Revolutionize Your IT Outsourcing</h1>
-                        <h2 className='text-white font-bold text-2xl text-center p-5 animate__animated animate__backInRight'>Unlock unparalleled efficiency and innovation with DevMarket's Strategic IT Partnerships</h2>
+                        <h2 className='text-white font-bold text-2xl text-center p-5 animate__animated animate__backInRight'>Unlock unparalleled efficiency and innovation with DevMarket&apos;s Strategic IT Partnerships</h2>
                         <div className='flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-10 pt-10 animate__animated animate__backInUp'>
                             <button className='bg-red-500 px-7 py-3 text-white font-semibold'>
                                 TRANSFORM Your IT TODAY
@@ -37,7 +37,7 @@ function Page() {
                     <div>
                         <div>
                             <h1 className='font-bold text-slate-700 text-4xl text-center'>The DevMarket Difference</h1>
-                            <p className='text-white px-4 sm:px-[10rem] lg:px-[20rem] py-4 text-center'>At DevMarket, we're not just another IT outsourcing company. We're your strategic partner in digital transformation, offering a unique blend of expertise, innovation, and collaboration that sets us apart from the rest.</p>
+                            <p className='text-white px-4 sm:px-[10rem] lg:px-[20rem] py-4 text-center'>At DevMarket, we&apos;re not just another IT outsourcing company. We&apos;re your strategic partner in digital transformation, offering a unique blend of expertise, innovation, and collaboration that sets us apart from the rest.</p>
                         </div>
                         <div className='flex flex-col sm:flex-row items-center justify-center gap-10 pt-10'>
                             <div className='flex flex-col items-center'>
@@ -63,7 +63,7 @@ function Page() {
                         <div className='flex flex-col lg:flex-row items-center justify-center gap-10 pt-10'>
                             <div className='flex flex-col items-center justify-center w-full max-w-xs p-8 h-auto border shadow-md rounded-md shadow-slate-300 hover:scale-105 transition-transform ease-linear'>
                                 <span className='text-2xl font-bold pb-4'>True Partnership</span>
-                                <p className='text-center'>We don't just work for you, we work with you. Our success is measured by your success.</p>
+                                <p className='text-center'>We don&apos;t just work for you, we work with you. Our success is measured by your success.</p>
                             </div>
                             <div className='flex flex-col items-center justify-center w-full max-w-xs p-8 h-auto border shadow-md rounded-md shadow-slate-300 hover:scale-105 transition-transform ease-linear'>
                                 <span className='text-2xl font-bold text-center pb-4'>End-to-End Solutions</span>
@@ -100,7 +100,7 @@ function Page() {
                         <h1 className='text-white text-3xl font-bold text-center'>Ready to Elevate Your IT Strategy?</h1>
                     </div>
                     <div>
-                        <p className='text-white text-lg text-center'>Experience the power of strategic IT partnership. Let's discuss how we can drive your business forward.</p>
+                        <p className='text-white text-lg text-center'>Experience the power of strategic IT partnership. Let&apos;s discuss how we can drive your business forward.</p>
                     </div>
                     <div>
                         <button className='bg-red-500 font-semibold text-white text-center px-4 py-2' data-cal-namespace="mock-interviews"
